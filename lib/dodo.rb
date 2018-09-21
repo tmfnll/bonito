27d31dc1925d5600616a449a03193f42d69252e1
Tom Finill <tomfinill@gmail.com>
2018-09-21 23:37:55 +0100

    Sketch out window and moment classes

--- lib/dodo.rb
+++ lib/dodo.rb
@@ -1,5 +1,89 @@
 require "dodo/version"
+require "securerandom"
 
 module Dodo
-  # Your code goes here...
+  module TimeWarp
+    class Window
+
+      attr_reader :starting, :duration
+
+      def initialize(duration, starting: 0, &block)
+        @duration = duration
+        @starting = starting
+        @offset = starting
+        @happenings = []
+        @total_child_duration = 0
+
+        instance_eval &block
+      end
+
+      def ending
+        starting + duration
+      end
+
+      def over(duration, &block)
+        self.class.new(duration, &block).tap do |window|
+          self << window
+        end
+      end
+
+      def now(&block)
+        Moment.new(&block).tap { |moment|  self << moment }
+      end
+
+      def repeat(times: 1, &block)
+        times.times { now &block }
+      end
+
+      def use(happening)
+        self << happening
+      end
+
+      def schedule(offset)
+        schedule = []
+
+        schedule.concat [@happenings, distribution].transpose do |happening, dist_offset|
+          offset += dist_offset
+          happening.schedule(offset).tap { |_moments| offset += happening.duration}
+        end
+
+        offset = ending
+        schedule
+      end
+
+      def << (happening)
+        @total_child_duration += happening.duration
+        raise Exception if @total_child_duration > duration
+        @happenings << happening
+      end
+
+      private
+
+      def distribution
+       @happenings.map do |_h|
+          SecureRandom.random_number (duration - @total_child_duration).to_i
+       end.sort
+      end
+    end
+
+    class Moment
+      def initialize(&block)
+        @block = block
+        @starting = nil
+      end
+
+      def schedule(offset)
+        @starting = offset
+        [self]
+      end
+
+      def ending
+        starting
+      end
+
+      def duration
+        0
+      end
+    end
+  end
 end
--- spec/dodo_spec.rb
+++ spec/dodo_spec.rb
@@ -1,9 +1,26 @@
+require "dodo"
+
 RSpec.describe Dodo do
   it "has a version number" do
     expect(Dodo::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    window = Dodo::TimeWarp::Window.new 20 do
+      now do
+        puts "first moment"
+      end
+      over 3 do
+        now do
+          puts "second moment"
+        end
+      end
+      now do
+        puts "third moment"
+      end
+    end
+    window.schedule 0
   end
+
+
 end
