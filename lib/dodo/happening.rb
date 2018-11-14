6a33df67135a3961dc268cfc5be2e505c3079e98
TomFinill <tom@kalohq.com>
2018-11-14 11:39:15 +0100

    move `OffsetHappening` to same file as `Happening`

--- lib/dodo/happening.rb
+++ lib/dodo/happening.rb
@@ -6,4 +6,16 @@ module Dodo
       @duration = duration
     end
   end
+
+  class OffsetHappening < SimpleDelegator
+    attr_reader :offset
+    def initialize(moment, offset)
+      @offset = offset
+      super moment
+    end
+
+    def ==(other)
+      offset == other.offset && __getobj__ == other.__getobj__
+    end
+  end
 end
--- lib/dodo/moment.rb
+++ lib/dodo/moment.rb
@@ -20,17 +20,7 @@ module Dodo
     end
     end
 
-  class OffsetHappening < SimpleDelegator
-    attr_reader :offset
-    def initialize(moment, offset)
-      @offset = offset
-      super moment
-    end
 
-    def ==(other)
-      offset == other.offset && __getobj__ == other.__getobj__
-    end
-  end
 
   class MomentEnumerator
     include Enumerable
