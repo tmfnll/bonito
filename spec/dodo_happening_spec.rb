b4f3cb03bc03ce5bb62c8d0aab02e60f0a7c5216
TomFinill <tom@kalohq.com>
2018-12-16 13:21:01 +0100

    Add tests for the ContextualMoment

---
+++ spec/dodo_happening_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe Dodo::ContextualMoment do
+
+  let(:moment) { build :moment }
+  let(:offset) { 2.weeks.ago }
+  let(:context) { Dodo::Context.new }
+  let(:contextual_moment) { described_class.new moment, offset, context}
+
+  describe '#evaluate' do
+    subject { contextual_moment.evaluate }
+
+    it 'should evaluate the moment within the context' do
+      allow(context).to receive(:instance_eval) do |&block|
+        expect(block).to eq(moment)
+      end
+    end
+
+    it "should evaluate the moment within the context at the offset" do
+      allow(context).to receive(:instance_eval) do |&_block|
+        expect(Time.now).to eq offset
+      end
+    end
+  end
+
+
+
+end
