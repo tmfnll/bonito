549c46b5d42a46965ec6f23e75289f94262e6fcf
TomFinill <tom@kalohq.com>
2019-04-07 23:04:15 +0200

    Make ScopedMoment tests meaningful
    A combination of a using `allow` instead of `expect` and not actually invoking the `subject` helper meant that the ScopedMoment tests were not really doing anything.  This fix ensures that they are and that instances of the class behave as expected.

--- spec/dodo_timeline_spec.rb
+++ spec/dodo_timeline_spec.rb
@@ -12,15 +12,17 @@ RSpec.describe Dodo::ScopedMoment do
     subject { scoped_moment.evaluate }
 
     it 'should evaluate the moment within the scope' do
-      allow(scope).to receive(:instance_eval) do |&block|
-        expect(block).to eq(moment)
+      expect(scope).to receive(:instance_eval) do |&block|
+        expect(block).to eq(moment.to_proc)
       end
+      subject
     end
 
     it 'should evaluate the moment within the scope at the offset' do
-      allow(scope).to receive(:instance_eval) do |&_block|
+      expect(scope).to receive(:instance_eval) do |&_block|
         expect(Time.now).to eq offset
       end
+      subject
     end
   end
 end
