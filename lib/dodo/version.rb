5f4b8b8b0a26984e2223896e8d94c156e7e6795e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-07 21:35:49 +0000

    Deploy TomFinill/dodo to github.com/TomFinill/dodo.git:gh-pages

--- lib/dodo/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Dodo
-  VERSION = '0.1.0'
-end
--- spec/dodo_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Dodo do
-  it 'has a version number' do
-    expect(Dodo::VERSION).not_to be nil
-  end
-end
